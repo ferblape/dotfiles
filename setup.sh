688887b15d61cc90e618b26fb9a234fd3878fb6e
Fernando Blat <fernando@blat.es>
2020-11-02 09:13:19 +0100

    setup

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ln -s $PWD/.ackrc ~/
+ln -s $PWD/.agignore ~/
+ln -s $PWD/.bash_login ~/
+ln -s $PWD/.curlrc ~/
+ln -s $PWD/.gemrc ~/
+ln -s $PWD/.gitconfig ~/
+ln -s $PWD/.gitignore ~/
+ln -s $PWD/.gitignore_global ~/
+ln -s $PWD/.irbrc ~/
+ln -s $PWD/.psqlrc ~/
+ln -s $PWD/.tmux.conf ~/
+ln -s $PWD/.git ~/
+ln -s $PWD/.bundle ~/
